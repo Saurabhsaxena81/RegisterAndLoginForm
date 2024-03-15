43ae96f2f1dccbbda40f9e8146fe4532fd299ed5
Saurabhsaxena81 <144423649+Saurabhsaxena81@users.noreply.github.com>
2024-03-15 12:38:19 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,28 +7,6 @@ const userSchema = mongoose.Schema({
   secret: String,
 });
 userSchema.plugin(plm);
-// const userSchema = new mongoose.Schema({
-//   username: String,
-//   nickname: String,
-//   description: String,
-//   categories: {
-//     type: Array,
-//     default: [],
-//   },
-//   datecreated: {
-//     type: Date,
-//     default: Date.now(),
-//   },
-// });
 
 module.exports = mongoose.model("user", userSchema);
 
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-// module.exports = router;
